27cb26acce20762a0fe3dcf9a3a18c8cace7a970
Rohit Tiwari <tigertiwari1023@gmail.com>
2025-07-17 22:52:56 +0530

    feat: switch for enabling select all on click

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,7 +4,9 @@ import React from "react";
 const LoadingPage = () => {
 	return (
 		<div className="w-screen dark h-screen flex items-center justify-center ">
-			<Loader2Icon className="size-8 animate-spin duration-500 text-purple-600" />
+			<div className="flex items-center justify-center w-8 h-8">
+				<Loader2Icon className="size-8 animate-spin duration-500 text-purple-600" />
+			</div>
 		</div>
 	);
 };
